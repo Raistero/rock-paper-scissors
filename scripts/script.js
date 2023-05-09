7785fa6c58b6c7511bd88968b70fea50ad26e226
Bogdan Ciuculescu <bogdan.ciuculescu@gmail.com>
2023-05-10 00:22:55 +0100

    Create function that generates computer choice
    Generate a random number between 0 and 99 and divide it in equal theree
    chunks with each taking an option.

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 100);
+  if (randomNumber <= 33) {
+    return "rock";
+  } else if (randomNumber <= 66) {
+    return "paper";
+  } else 
+    return "scissors";
+}
