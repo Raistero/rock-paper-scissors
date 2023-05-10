276b25ba44b3506f2ae003b3218a85e9f920d9d9
Bogdan Ciuculescu <bogdan.ciuculescu@gmail.com>
2023-05-10 22:16:04 +0100

    Create interface to play 5 games. Print the result Add a case for an incorrect user input and print an appropriate console message.
    Create a helper function that returns a "W", "L" or "D" based on the
    result of the game.
    
    Iterate 5 times and increment a counter for each result and print it.
    Declare the game winner and print the score.

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
   playerSelection = playerSelection.at(0).toUpperCase() + playerSelection.substring(1).toLowerCase();
   if (playerSelection === computerSelection) {
-    return `It's a draw`;
+    return `It's a draw!`;
   } else if (playerSelection === "Rock" && computerSelection === "Paper"){
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
@@ -22,7 +22,48 @@ function playRound(playerSelection, computerSelection) {
     return `You Win! ${playerSelection} beats ${computerSelection}`;
   } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
-  } else {
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
     return `You Win! ${playerSelection} beats ${computerSelection}`;
+  } else {
+    return "Incorrect choice entered";
   }
+}
+
+function getGameResult (result) {
+  if (result.charAt(4) === "W") {
+    return "W";
+  } else if(result.charAt(4) === "L") {
+    return "L";
+  } else {
+    return "D";
+  }
+}
+
+let winCounter = 0;
+let loseCounter = 0;
+let drawCounter = 0
+
+for(let i = 1; i <= 5; i++) {
+  let playerChoice = prompt("Insert your choice:", "Rock, Paper or Scissors");
+  let roundPlay = playRound(playerChoice, getComputerChoice());
+  if (getGameResult(roundPlay) === "W") {
+    console.log(roundPlay);
+    winCounter++;
+  } else if(getGameResult(roundPlay) === "L") {
+    console.log(roundPlay);
+    loseCounter++;
+  } else if (getGameResult(roundPlay) === "D"){
+    console.log(roundPlay);
+    drawCounter++;
+  } else {
+    console.log(roundPlay);
+  }
+}
+
+if(winCounter > loseCounter) {
+  console.log(`You Won! Result: You ${winCounter} - ${loseCounter} Computer (${drawCounter} Draw)`);
+} else if (winCounter < loseCounter) {
+  console.log(`You Lost! Result: You ${winCounter} - ${loseCounter} Computer (${drawCounter} Draw)`);
+} else {
+  console.log(`It's a Draw! Result: You ${winCounter} - ${loseCounter} Computer`);
 }
