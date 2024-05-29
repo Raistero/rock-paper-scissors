34ba827975fe6c3e7f1e40de846a785c6b4a11a0
Bogdan Ciuculescu <bogdan.ciuculescu@gmail.com>
2024-05-29 13:52:41 +0100

    Add logic for every possibility and return the result of one round

--- script/script.js
+++ script/script.js
@@ -12,5 +12,39 @@ function getComputerChoice() {
 }
 
 function getHumanChoice(){
-  return result = prompt("Please write your choice(rock, paper or scissors): ").toLowerCase().trim();
+  return prompt("Please write your choice(rock, paper or scissors): ").toLowerCase().trim();
 }
+
+function playRound(humanChoice, computerChoice) {
+  if(humanChoice === "rock" && computerChoice === "paper") {
+    computerScore += 1;
+    return "Computer Wins! Paper beats Rock";
+  } else if (humanChoice === "rock" && computerChoice === "scissors") {
+    humanScore += 1;
+    return "You Win! Rock beats Scissors";
+  } else if (humanChoice === "rock" && computerChoice === "rock")
+    return "It's a Draw!";
+  
+  if(humanChoice === "paper" && computerChoice === "scissors") {
+    computerScore += 1;
+    return "Computer Wins! Scissors beats Paper";
+  } else if (humanChoice === "paper" && computerChoice === "rock") {
+    humanScore += 1;
+    return "You Win! Paper beats Rock";
+  } else if (humanChoice === "paper" && computerChoice === "paper")
+    return "It's a Draw!";
+
+  if(humanChoice === "scissors" && computerChoice === "rock") {
+    computerScore += 1;
+    return "Computer Wins! Rock beats Scissors";
+  } else if (humanChoice === "scissors" && computerChoice === "paper") {
+    humanScore += 1;
+    return "You Win! Scissors beats Paper";
+  } else if (humanChoice === "scissors" && computerChoice === "scissors")
+    return "It's a Draw!";
+}
+
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice();
+
+console.log(playRound(humanChoice, computerChoice));
