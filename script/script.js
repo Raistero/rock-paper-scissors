7ba2c1517a6f1911e432bb22ef165df0410b35da
Bogdan Ciuculescu <bogdan.ciuculescu@gmail.com>
2024-07-04 00:06:39 +0100

    Create logic for each button
    Upon reaching 5 points, the log will declare the winner. When reaching
    that stage, every button has been disabled.
    New game button has been added. When selected, the log will be cleared
    and the options will be re-enabled.

--- script/script.js
+++ script/script.js
@@ -10,28 +10,28 @@ function getComputerChoice() {
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === "rock" && computerChoice === "paper") {
-    computerScore += 1;
+    computerScore++;
     return "Computer Wins! Paper beats Rock";
   } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    humanScore += 1;
+    humanScore++;
     return "You Win! Rock beats Scissors";
   } else if (humanChoice === "rock" && computerChoice === "rock")
     return "It's a Draw!";
 
   if (humanChoice === "paper" && computerChoice === "scissors") {
-    computerScore += 1;
+    computerScore++;
     return "Computer Wins! Scissors beats Paper";
   } else if (humanChoice === "paper" && computerChoice === "rock") {
-    humanScore += 1;
+    humanScore++;
     return "You Win! Paper beats Rock";
   } else if (humanChoice === "paper" && computerChoice === "paper")
     return "It's a Draw!";
 
   if (humanChoice === "scissors" && computerChoice === "rock") {
-    computerScore += 1;
+    computerScore++;
     return "Computer Wins! Rock beats Scissors";
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    humanScore += 1;
+    humanScore++;
     return "You Win! Scissors beats Paper";
   } else if (humanChoice === "scissors" && computerChoice === "scissors")
     return "It's a Draw!";
@@ -42,26 +42,86 @@ function resultsLogPrint(userInput) {
   resultsLog.textContent = `${playRound(
     userInput,
     getComputerChoice()
-  )} Current score: You ${humanScore} - ${computerScore} Computer`;
-  document.body.appendChild(resultsLog);
+  )}. Current score: You ${humanScore} - ${computerScore} Computer`;
+  document.querySelector(".results-log-container").appendChild(resultsLog);
+}
+
+function printFinalScore() {
+  const finalScorePrint = document.createElement("p");
+  if (computerScore > humanScore) {
+    finalScorePrint.textContent = `Computer Won! The final score is: You ${humanScore} - ${computerScore} Computer`;
+    document
+      .querySelector(".results-log-container")
+      .appendChild(finalScorePrint);
+  } else {
+    finalScorePrint.textContent = `You Won! The final score is: You ${humanScore} - ${computerScore} Computer`;
+    document
+      .querySelector(".results-log-container")
+      .appendChild(finalScorePrint);
+  }
+}
+
+function endGame() {
+  rockButton.disabled = true;
+  paperButton.disabled = true;
+  scissorsButton.disabled = true;
+  newGameButton.style.display = "inline";
 }
 
 const rockButton = document.querySelector("#rock-btn");
 const paperButton = document.querySelector("#paper-btn");
 const scissorsButton = document.querySelector("#scissors-btn");
+const newGameButton = document.querySelector("#new-game");
 
+//Every handler function will print the result of a game, after a click, if any of the score is less then 5.
+//It will then check if it reaches 5 and if it does, it prints the final result of the game.
 function handledRock() {
-  resultsLogPrint("rock");
+  if (computerScore < 5 && humanScore < 5) {
+    resultsLogPrint("rock");
+  }
+
+  if (computerScore == 5 || humanScore == 5) {
+    printFinalScore();
+    // rockButton.removeEventListener("click", handledRock);
+    endGame()
+  }
 }
 
 function handledPaper() {
-  resultsLogPrint("paper");
+  if (computerScore < 5 && humanScore < 5) {
+    resultsLogPrint("paper");
+  }
+
+  if (computerScore == 5 || humanScore == 5) {
+    printFinalScore();
+    // rockButton.removeEventListener("click", handledPaper);
+    endGame()
+  }
 }
 
 function hanndledScissors() {
-  resultsLogPrint("scissors");
+  if (computerScore < 5 && humanScore < 5) {
+    resultsLogPrint("scissors");
+  }
+
+  if (computerScore == 5 || humanScore == 5) {
+    printFinalScore();
+    // rockButton.removeEventListener("click", hanndledScissors);
+    endGame()
+  }
+}
+
+function handledNewGame() {
+  document.querySelector(".results-log-container").innerHTML = "";
+  newGameButton.style.display = "none";
+  rockButton.disabled = false;
+  paperButton.disabled = false;
+  scissorsButton.disabled = false;
+  humanScore = 0;
+  computerScore = 0;
 }
 
 rockButton.addEventListener("click", handledRock);
 paperButton.addEventListener("click", handledPaper);
 scissorsButton.addEventListener("click", hanndledScissors);
+newGameButton.addEventListener("click", handledNewGame);
